55cc41beddf62d9cdae0d4302b81ec7b5e143b13
waseem3423 <waseem3423@users.noreply.github.com>
2026-08-24 19:27:11 +0500

    Clean up react-v folder from main HTML repo

--- react-v/js/data/home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// ==================[ Personal Data ]================== //
-export const my_name = "Waliha Idrees"; // must write first name + last name
-
-export const short_description =
-  "“I design creative, eye-catching graphics and visual brand experiences that help brands stand out.”";
-// ==================[ Personal Data ]================== //
--- react-v/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Portfolio from './pages/Portfolio.jsx';
-
-export default function App() {
-  return (
-    <Portfolio />
-  );
-}
