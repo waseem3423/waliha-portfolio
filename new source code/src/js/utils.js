264215a08275354bfc5ea719cc6f9b3b529bdf38
waseem3423 <waseem3423@users.noreply.github.com>
2026-08-24 14:29:30 +0500

    Initial commit: Waliha Idrees Graphics Designer Portfolio

--- new source code/src/js/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function select(className) {
-  return document.querySelector(className);
-}
-
-function selectAll(className) {
-  return [...document.querySelectorAll(className)];
-}
